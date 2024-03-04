fc2d9d18415c46d0bd2c646982829b8ba7b59cfd
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2024-03-04 12:15:45 +0100

    add

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -2,15 +2,15 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const OrderSummary = ({ submitForm }) => {
-
-    const cart = useSelector((state) => state.cart.cart);
-    console.log('cart: ', cart);
+  const cart = useSelector((state) => state.cart.cart);
+  console.log("cart: ", cart);
+  const total = cart.reduce((sum, item) => sum + item.quantity * item.price, 0);
   return (
     <div>
-      <p> Order summary</p>
+      <p> Order summary : {total}</p>
       <button
         type="button"
-        onClick={submitForm} 
+        onClick={submitForm}
         className="w-32 bg-blue-500 text-white p-2 rounded hover:bg-blue-600 disabled:opacity-50 mt-4"
       >
         Submit
