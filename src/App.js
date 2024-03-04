78c636286d27012f2cf938567bbc5785ad3fc22e
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2024-03-04 11:49:20 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Routes, Route } from "react-router-dom";
 import ShopsPage from "./pages/ShopsPage";
 import CartPage from "./pages/CartPage";
 import Layout from "components/Layout/Layout";
-import CartSuccessPage from "./pages/CartSuccessPage";
+import CartSuccessPage from "pages/CartSuccessPage";
 
 function App() {
   return (
-    <Router basename="/medicine_delivery">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<ShopsPage />} />
-          <Route path="cart" element={<CartPage />} />
-          <Route path="success" element={<CartSuccessPage />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<ShopsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/success" element={<CartSuccessPage />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-
+import { BrowserRouter } from "react-router-dom";
 import { persistor, store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,8 +12,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-
+        <BrowserRouter basename="/medicine_delivery">
           <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
